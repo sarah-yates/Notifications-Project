338097438be2c06e6a3523a719c15722152b00b9
sarahafreeze <sarahafreeze@gmail.com>
2020-04-30 13:46:59 -0500

    Changed the design, notification will now dismiss when user clicks it, trying to get the app to open when user click notification -Sarah

--- app/src/main/java/com/example/notificationproject/MainActivity.kt
+++ app/src/main/java/com/example/notificationproject/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.RemoteViews
 import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationManagerCompat
 import com.example.notificationproject.R
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -19,7 +20,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var notificationChannel: NotificationChannel
     lateinit var builder: Notification.Builder
     private val channelID = "com.example.notification"
-    private val description =  "Test notification"
+    private val description = "Test notification"
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,29 +28,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-        btn_notify.setOnClickListener{
+        btn_notify.setOnClickListener {
 
-            val intent = Intent(this, LauncherActivity::class.java)
-            val pendingIntent = PendingIntent.getActivity(this,0,intent, PendingIntent.FLAG_UPDATE_CURRENT)
+            val intent = Intent(this, LauncherActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }
+            val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
 
-            val contentView = RemoteViews(packageName,R.layout.notification_layout)
-            contentView.setTextViewText(R.id.tv_title,"Sarah!")
+
+            val contentView = RemoteViews(packageName, R.layout.notification_layout)
+            contentView.setTextViewText(R.id.tv_title, "Title here")
             contentView.setTextViewText(R.id.tv_content, "Test Notification")
 
-            notificationChannel = NotificationChannel(channelID,description,NotificationManager.IMPORTANCE_HIGH)
+            notificationChannel =
+                NotificationChannel(channelID, description, NotificationManager.IMPORTANCE_HIGH)
             notificationChannel.enableLights(true)
-            notificationChannel.lightColor = Color.GREEN
+            notificationChannel.lightColor = Color.RED
             notificationChannel.enableVibration(false)
             notificationManager.createNotificationChannel(notificationChannel)
 
-            builder = Notification.Builder(this,channelID)
-                    .setContent(contentView)
-                    .setSmallIcon(R.drawable.ic_launcher_round)
-                    .setLargeIcon(BitmapFactory.decodeResource(this.resources,R.drawable.ic_launcher))
-                    .setContentIntent(pendingIntent)
+            builder = Notification.Builder(this, channelID)
+                .setContent(contentView)
+                .setSmallIcon(R.drawable.notification_icon)
+                //.setLargeIcon(BitmapFactory.decodeResource(this.resources, R.drawable.notification_icon))
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
 
 
-            notificationManager.notify(1234,builder.build())
+            with(NotificationManagerCompat.from(this)) {
+                //notificationId is a unique int for each notification that you must define
+                notify(1234, builder.build())
+            }
         }
     }
 }
