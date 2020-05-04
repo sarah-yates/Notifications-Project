832cf0aece35b68c9084609d1ff7a1e356b2a7b1
sarahafreeze <sarahafreeze@gmail.com>
2020-05-04 17:24:07 -0500

    Third Commit, added a service and moved notification code into MyService.kt file -Sarah

--- app/src/main/java/com/example/notificationproject/MainActivity.kt
+++ app/src/main/java/com/example/notificationproject/MainActivity.kt
@@ -16,49 +16,17 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var notificationManager: NotificationManager
-    lateinit var notificationChannel: NotificationChannel
-    lateinit var builder: Notification.Builder
-    private val channelID = "com.example.notification"
-    private val description = "Test notification"
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         btn_notify.setOnClickListener {
+            //creates intent to start the service
+            val intent = Intent(this,MyService::class.java)
+            startService(intent)
 
-            val intent = Intent(this, LauncherActivity::class.java).apply {
-                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            }
-            val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
-
-
-            val contentView = RemoteViews(packageName, R.layout.notification_layout)
-            contentView.setTextViewText(R.id.tv_title, "Title here")
-            contentView.setTextViewText(R.id.tv_content, "Test Notification")
-
-            notificationChannel =
-                NotificationChannel(channelID, description, NotificationManager.IMPORTANCE_HIGH)
-            notificationChannel.enableLights(true)
-            notificationChannel.lightColor = Color.RED
-            notificationChannel.enableVibration(false)
-            notificationManager.createNotificationChannel(notificationChannel)
-
-            builder = Notification.Builder(this, channelID)
-                .setContent(contentView)
-                .setSmallIcon(R.drawable.notification_icon)
-                //.setLargeIcon(BitmapFactory.decodeResource(this.resources, R.drawable.notification_icon))
-                .setContentIntent(pendingIntent)
-                .setAutoCancel(true)
-
-
-            with(NotificationManagerCompat.from(this)) {
-                //notificationId is a unique int for each notification that you must define
-                notify(1234, builder.build())
-            }
-        }
+       }
     }
 }
---
+++ app/src/main/java/com/example/notificationproject/MyService.kt
@@ -0,0 +1,68 @@
+package com.example.notificationproject
+
+import android.app.*
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
+import android.os.IBinder
+import android.util.EventLogTags
+import android.util.Log
+import android.widget.RemoteViews
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationManagerCompat
+
+class MyService : Service() {
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        if (intent != null) {
+            //do work that service needs to do
+            makeNotification()
+        }
+        return Service.START_STICKY
+    }
+
+    override fun onBind(intent: Intent): IBinder? {
+        return null
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun makeNotification() {
+        var notificationChannel: NotificationChannel
+        var notificationManager: NotificationManager
+        var builder: Notification.Builder
+        val channelID = "com.example.notificationproject"
+        val description = "Test notification"
+
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        val intent = Intent(this, LauncherActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+
+        val contentView = RemoteViews(packageName, R.layout.notification_layout)
+        contentView.setTextViewText(R.id.tv_title, "Title here")
+        contentView.setTextViewText(R.id.tv_content, "Test Notification")
+
+        notificationChannel =
+            NotificationChannel(channelID, description, NotificationManager.IMPORTANCE_HIGH)
+        notificationChannel.enableLights(true)
+        notificationChannel.lightColor = Color.RED
+        notificationChannel.enableVibration(false)
+        notificationManager.createNotificationChannel(notificationChannel)
+
+        builder = Notification.Builder(this, channelID)
+            .setContent(contentView)
+            .setSmallIcon(R.drawable.notification_icon)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+
+        with(NotificationManagerCompat.from(this)) {
+            //notificationId is a unique int for each notification that you must define
+            notify(1234, builder.build())
+        }
+    }
+}
